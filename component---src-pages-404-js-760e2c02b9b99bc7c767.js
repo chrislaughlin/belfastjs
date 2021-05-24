2690b3f35a500cfa3adf551759a2a5eb72235329
chrislaughlin <chrislaughlin@users.noreply.github.com>
2021-05-24 21:33:40 +0000

    deploy: 2a5d949d19e09e17b8165894b4cab948d4724cba

--- component---src-pages-404-js-760e2c02b9b99bc7c767.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkbelfastjs=self.webpackChunkbelfastjs||[]).push([[883],{9616:function(t,e,a){"use strict";a.r(e);var n=a(7294),l=a(7891),s=a(3751);e.default=function(t){var e=t.data,a=t.location,u=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:u},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-760e2c02b9b99bc7c767.js.map
